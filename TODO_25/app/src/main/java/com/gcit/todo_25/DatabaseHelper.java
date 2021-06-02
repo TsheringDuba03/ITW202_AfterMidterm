e52a6f44139eedd4f835b479f05aea635022ac27
TsheringDuba03 <12190094.gcit@rub.edu.bt>
2021-06-02 21:05:35 +0600

    TODO

--- TODO_25/app/src/main/java/com/gcit/todo_25/DatabaseHelper.java
+++ TODO_25/app/src/main/java/com/gcit/todo_25/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.gcit.todo_25;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -46,6 +47,32 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return true;
         }
 
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME, null);
+        return res;
+    }
+
+    public boolean updateData(String id, String name,String surname,String marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1,id);
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,surname);
+        contentValues.put(COL_4,marks);
+        long result = db.update(TABLE_NAME, contentValues, "ID= ?",
+                new String[]{id});
+        if (result != 0){
+            return true;
+        }
+        return false;
+    }
+
+    public Integer deleteData(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[]{id});
     }
 
 
--- TODO_25/app/src/main/java/com/gcit/todo_25/MainActivity.java
+++ TODO_25/app/src/main/java/com/gcit/todo_25/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gcit.todo_25;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -47,11 +49,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ViewData(View view) {
+        Cursor res = myDb.getAllData();
+        if (res.getCount() == 0){
+            showMessage("Error", "Nothing found");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()){
+            buffer.append("Student ID : "+ res.getString(0)+"\n");
+            buffer.append("First Name : "+ res.getString(1)+"\n");
+            buffer.append("Last Name : "+ res.getString(2)+"\n");
+            buffer.append("Marks : "+ res.getString(3)+"\n\n");
+        }
+
+        showMessage("List of students",buffer.toString());
+    }
+
+    public  void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 
     public void UpdateData(View view) {
+        boolean isUpdate = myDb.updateData(Id.getText().toString(),
+                FName.getText().toString(),
+                LName.getText().toString(),Marks.getText().toString());
+        if (isUpdate == true)
+            Toast.makeText(MainActivity.this,"Data Updated",Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(MainActivity.this,"Data not Updated",Toast.LENGTH_LONG).show();
     }
 
     public void DeleteData(View view) {
+        Integer deletedRows = myDb.deleteData(Id.getText().toString());
+        if (deletedRows > 0)
+            Toast.makeText(MainActivity.this, "Data Deleted",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(MainActivity.this, "Data  not Deleted",Toast.LENGTH_SHORT).show();
     }
 }
